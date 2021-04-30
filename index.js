959894c2da36d9fb3716f3b0506644e408a5876f
Artiminial <jeremiahedu2019@gmail.com>
2021-04-30 15:55:59 -0600

    site further updated to include date and time as well as more css styling

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
-let d = new Date();
-let year = d.getFullYear();
+let date = new Date();
+let year = date.getFullYear();
 document.getElementById("currentYear").textContent = year;
+let lastUpdated = date.getMonth() + "/" + date.getDate() + "/" + date.getFullYear() + "   " + date.toTimeString().split(" ")[0];
+document.getElementById("editDate").textContent = lastUpdated;
