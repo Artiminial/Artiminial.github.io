25763add2c5e84bf701391ac0de2dc2e3ba4062f
Artiminial <jeremiahedu2019@gmail.com>
2021-05-24 15:15:38 -0600

    This is the progress I made this week

---
+++ Lesson_4/preston_4.js
@@ -0,0 +1,11 @@
+let date = new Date();
+let year = date.getFullYear();
+document.getElementById("currentYear").textContent = year;
+let lastUpdated = date.getMonth() + "/" + date.getDate() + "/" + date.getFullYear() + "   " + date.toTimeString().split(" ")[0];
+document.getElementById("editDate").textContent = lastUpdated;
+
+function toggleMenue(){
+    document
+    .getElementsByClassName("hlinks")[0]
+    .classList.toggle("responsive");
+}
