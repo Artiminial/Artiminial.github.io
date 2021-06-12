5929b49181120653d96b3e1f3e9b5ca4994bc369
Artiminial <jeremiahedu2019@gmail.com>
2021-06-12 15:46:24 -0600

    This is the complete html for the project

---
+++ Lesson-8/js/preston_6.js
@@ -0,0 +1,37 @@
+let date = new Date();
+const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec']
+const days = ['Sunday', 'Monday', 'Teusday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+
+
+let currentMonth = months[date.getMonth()]
+let currentDay = days[date.getDay()]
+let currentYear = date.getFullYear()
+document.getElementById("currentDate").innerHTML = `${currentDay}, ${currentMonth}, ${currentYear} `;
+
+window.addEventListener('load', () => {
+    const hambutton = document.querySelector(".hamburgerMenue");
+    const mainnav = document.querySelector(".hlinks");
+
+    hambutton.addEventListener(
+        "click",
+        (event) => {
+          event.preventDefault()
+            mainnav.classList.toggle("responsive");
+        },
+        false
+    );
+})
+
+
+
+
+
+
+
+
+if (date.getDay() !== 5) {
+    document.getElementById("pancake-banner").className = 'hidden'
+} else {
+    document.getElementById("pancake-banner").className = 'show'
+
+}
