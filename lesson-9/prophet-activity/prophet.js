2bc7daf88f96f45924a1db16682b4acb02e3f43b
Artiminial <jeremiahedu2019@gmail.com>
2021-06-17 20:46:35 -0600

    This is the code for the prophet activity

---
+++ lesson-9/prophet-activity/prophet.js
@@ -0,0 +1,45 @@
+const requestURL = 'https://byui-cit230.github.io/lessons/lesson-09/data/latter-day-prophets.json';
+fetch(requestURL)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (jsonObject) {
+    console.table(jsonObject); 
+  });
+
+  async function fetchData(url) {
+    // console.log(url)
+    const response = await fetch(url)
+       .then(response => response.json())
+       .catch(error => console.log(error))
+  return response;
+  }
+
+  const getData = (url) => {
+    let encodedURI = encodeURI(url);
+    fetchData(encodedURI)
+        .then( data => {
+            displayData(data);
+    }).catch(error => {
+            console.log(error)})
+};
+
+
+
+
+
+    const displayData = (data) => {
+        const prophets = document.getElementById('prophets');
+        console.log(data)
+        data.prophets.forEach((element) => {
+        prophets.innerHTML += 
+        `<div class = "cards">
+        <h1>${element.name}${element.lastname} - ${element.order}</h1>
+        <p>Founded in ${element.birthdate}</p>
+        <p>${element.birthplace}</p>
+        <img class="image" src="${element.imageurl}" alt="Photo of ${element.name}">
+        </div>
+    `
+      })
+    };
+    getData(requestURL);
