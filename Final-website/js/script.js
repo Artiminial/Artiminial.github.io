b2830ec0007cfd8e4883afc8235995845596390a
Artiminial <jeremiahedu2019@gmail.com>
2021-07-14 21:47:04 -0600

    Got display data working

--- Final-website/js/script.js
+++ Final-website/js/script.js
@@ -1,4 +1,5 @@
 let url = "https://api.spoonacular.com/recipes/99999/information?apiKey=7a3793a6d2a74c46a5ff81a2da0cffd9&includeNutrition=true."
+
 /**
  * Returns a random integer between min (inclusive) and max (inclusive).
  * The value is no lower than min (or the next integer greater than min
@@ -6,6 +7,7 @@ let url = "https://api.spoonacular.com/recipes/99999/information?apiKey=7a3793a6
  * lower than max if max isn't an integer).
  * Using Math.round() will give you a non-uniform distribution!
  */
+
 function getRandomInt(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
@@ -27,15 +29,21 @@ async function fetchData(url){
     const response = await fetch(url).then((response) =>{
         return response.json();
     })
-
+    
     .catch((error) => console.log(error));
     return response;
 }
 
 function urlResult(){
     let rand = getRandomInt(100,000, 120,000);
-    getData(`https://api.spoonacular.com/recipes/${rand}/information?apiKey=7a3793a6d2a74c46a5ff81a2da0cffd9&includeNutrition=true.`) 
+    getData(`https://api.spoonacular.com/recipes/${rand}/information?apiKey=7a3793a6d2a74c46a5ff81a2da0cffd9&includeNutrition=true.`);
+    alert("IS connected?"); 
 }
 function displayData(data){
     console.log(data);
 }
+
+
+
+
+const button = document.getElementById('randomButton').addEventListener("click", urlResult());
